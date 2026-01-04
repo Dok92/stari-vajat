e4c9bad180d2100ee16aed85605af668f25961d4
Dok92 <doslovdok@gmail.com>
2026-01-04 18:33:14 +0100

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,23 @@
+// @ts-nocheck
+import { defineConfig } from 'astro/config';
+
+import solidJs from '@astrojs/solid-js';
+import vercel from '@astrojs/vercel';
+import tailwindcss from '@tailwindcss/vite';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [solidJs()],
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  devToolbar: {
+    enabled: false,
+  },
+});
